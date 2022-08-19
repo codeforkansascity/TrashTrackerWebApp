2c912581072315bf53f8049479740641d741c168
lulu <lulu.cao.her@gmail.com>
2022-08-19 15:27:33 -0500

    imported a simpler template for crudDB

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -28,10 +28,21 @@ export type AmplifyDependentResourcesAttributes = {
             "Arn": "string",
             "Region": "string",
             "LambdaExecutionRole": "string"
+        },
+        "crudDynamoDB2": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
         }
     },
     "api": {
         "webappREST": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        },
+        "apifdee16f8": {
             "RootUrl": "string",
             "ApiName": "string",
             "ApiId": "string"
@@ -46,6 +57,14 @@ export type AmplifyDependentResourcesAttributes = {
             "PartitionKeyType": "string",
             "SortKeyName": "string",
             "SortKeyType": "string",
+            "Region": "string"
+        },
+        "twilio2": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
             "Region": "string"
         }
     }
