5545797805bae5f4294bddc4e3e608a6ed2bb620
lulu <lulu.cao.her@gmail.com>
2022-09-10 12:29:46 -0500

    trying to fix bug in map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -3,8 +3,8 @@ import { MapView, LocationSearch } from '@aws-amplify/ui-react';
 
 import '@aws-amplify/ui-react/styles.css';
 
-export default function BasicMap() {
-  return <MapView id="map" initialViewState={{
+export default function Map() {
+  return <MapView initialViewState={{
         latitude: 39.1002489,
         longitude: -94.5340805,
         zoom: 14,
