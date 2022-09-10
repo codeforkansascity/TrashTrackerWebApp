488e2c4a6bee623a03ecbdb237ba04249e3d823c
lulu <lulu.cao.her@gmail.com>
2022-09-10 13:56:56 -0500

    updated map style

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -66,6 +66,12 @@ export type AmplifyDependentResourcesAttributes = {
             "Style": "string",
             "Region": "string",
             "Arn": "string"
+        },
+        "map2": {
+            "Name": "string",
+            "Style": "string",
+            "Region": "string",
+            "Arn": "string"
         }
     }
 }
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -14,3 +14,44 @@ const Map = () => {
 };
 
 export default Map;
+
+// import { createMap } from "maplibre-gl-js-amplify";
+// import "maplibre-gl/dist/maplibre-gl.css";
+// import { drawPoints } from "maplibre-gl-js-amplify";
+
+// async function initializeMap() {
+//     const map = await createMap({
+//         container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
+//         center: [-123.1187, 49.2819], // [Longitude, Latitude]
+//         zoom: 11,
+//     })
+// }
+
+// initializeMap();
+
+// export default ...
+
+// map.on("load", function () {
+//     drawPoints("mySourceName", // Arbitrary source name
+//         [
+//             {
+//               coordinates: [-122.483696, 37.833818], // [Longitude, Latitude]
+//               title: "Golden Gate Bridge",
+//               address: "A suspension bridge spanning the Golden Gate",
+//             },
+//             {
+//               coordinates: [- 122.4770, 37.8105], // [Longitude, Latitude]
+//             },
+//         ], // An array of coordinate data, an array of Feature data, or an array of [NamedLocations](https://github.com/aws-amplify/maplibre-gl-js-amplify/blob/main/src/types.ts#L8)
+//         map,
+//         {
+//             showCluster: true,
+//             unclusteredOptions: {
+//                 showMarkerPopup: true,
+//             },
+//             clusterOptions: {
+//                 showCount: true,
+//             },
+//         }
+//     );
+// });
