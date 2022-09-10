520aa73f9f89cf2f8964e733ab0e2ab00113a7e1
lulu <lulu.cao.her@gmail.com>
2022-09-10 13:11:17 -0500

    trying to fix bug in map

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,14 +2,16 @@ import React from 'react';
 import Datafilter from './Datafilter';
 import Datatable5 from './Datatable5';
 import Footer from './Footer';
-import PrintMap from './PrintMap';
+// import PrintMap from './PrintMap';
+import Map from './Map';
 
 const Home = () => {
     return (
         <div className="custom-container">
+            <Map />
             <Datafilter />
             <Datatable5 />
-            <PrintMap />
+            {/* <PrintMap /> */}
             <Footer />
         </div>
     );
--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { MapView, LocationSearch } from '@aws-amplify/ui-react';
-
 import '@aws-amplify/ui-react/styles.css';
+import './Map.css';
 
-export default function Map() {
-  return <MapView initialViewState={{
+const Map = () => {
+  return <MapView id="custom-map" initialViewState={{
         latitude: 39.1002489,
         longitude: -94.5340805,
         zoom: 14,
     }}>
         <LocationSearch position="top-left"/>
     </MapView>
-}
+};
+
+export default Map;
