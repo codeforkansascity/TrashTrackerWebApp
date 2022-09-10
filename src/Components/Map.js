50996fb3e2326112659bbc35aeab388198ee3d55
lulu <lulu.cao.her@gmail.com>
2022-09-10 11:53:43 -0500

    trying to fix bug in map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -4,13 +4,11 @@ import { MapView, LocationSearch } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 export default function BasicMap() {
-  return <div>
-        <MapView id="map" initialViewState={{
-            latitude: 39.1002489,
-            longitude: -94.5340805,
-            zoom: 14,
-        }}>
-            <LocationSearch position="top-left"/>
-        </MapView>
-    </div>
+  return <MapView id="map" initialViewState={{
+        latitude: 39.1002489,
+        longitude: -94.5340805,
+        zoom: 14,
+    }}>
+        <LocationSearch position="top-left"/>
+    </MapView>
 }
