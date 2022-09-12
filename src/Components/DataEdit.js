f01762c8bb9344023fd30d2260bd3e4b74649827
lulu <lulu.cao.her@gmail.com>
2022-09-12 18:49:53 -0500

    updated placeholder for editing reports

--- src/Components/DataEdit.js
+++ src/Components/DataEdit.js
@@ -1,71 +1,87 @@
-import React from "react";
+// import React from "react";
 
-class DataEdit extends React.Component {
-  constructor(props) {
-    super(props);
+// class DataEdit extends React.Component {
+//   constructor(props) {
+//     super(props);
 
-    this.state = {
-      items: [],
-      DataisLoaded: false
-    };
+//     this.state = {
+//       items: [],
+//       DataisLoaded: false
+//     };
 
-      this.handleChange = this.handleChange.bind(this);
-}
+//     this.handleChange = this.handleChange.bind(this);
+//     this.submit = this.submit.bind(this);
+//   }
 
-  handleChange(event) {
-      const name = event.target.name;
-      this.setState({[name]: event.target.value});
-  }
+//   componentDidMount() {
+//     fetch("https://9gdq2gvn61.execute-api.us-east-2.amazonaws.com/staging/twilio/body")
+//       .then((res) => res.json())
+//       .then((reports) => {
+//         this.setState({
+//           items: reports,
+//           report: '',
+//           DataisLoaded: true
+//         });
+//       })
+//   }
 
-componentDidMount() {
-  fetch("https://9gdq2gvn61.execute-api.us-east-2.amazonaws.com/staging/twilio/body")
-    .then((res) => res.json())
-    .then((reports) => {
-      this.setState({
-        items: reports,
-        trash_name: reports.trash_name,
-        DataisLoaded: true
-      });
-    })
-}
+//   handleChange(event) {
+//       const name = event.target.name;
+//       this.setState({[name]: event.target.value});
+//   }
 
-render() {
-  const { DataisLoaded, items } = this.state;
-  if (!DataisLoaded) return 
-      <div>
-    <h1> Pleses wait some time.... </h1> 
-      </div>
+//   setReports(e) {
+//     console.log("testing")
+//   }
 
-  return (
-    <section>
-      <br/><br/><br/>
-      <h2>Reports Edit</h2>
-      {
-        items.map((item) => (
-            <form onSubmit={this.handleSubmit}>
-                <label><b>Report:&nbsp;</b>
-                    <input name="photo_url" type="text" value={item.photo_url} onChange={this.handleChange} />
-                </label>
-                <lable>
-                    <input name="trash_name" type="text" value={this.state.trash_name} onChange={this.handleChange} />
-                </lable>
-                <label> 
-                    <input name="location" type="text" value={item.location.slice(9)} onChange={this.handleChange} />
-                </label>
-                <label>
-                    <input name="report_from" type="text" value={item.report_from.slice(2,5) + "-" + item.report_from.slice(5,8) + "-" + item.report_from.slice(8)} onChange={this.handleChange} />
-                </label>
-                <label>
-                    <input name="report_date" type="text" value={item.report_date.slice(0, 10)} onChange={this.handleChange} />
-                </label>
+//   submit(event) {
+//   fetch("https://9gdq2gvn61.execute-api.us-east-2.amazonaws.com/staging/twilio/body", {
+//     method: 'POST',
+//       body: JSON.stringify({ report }),
+//       headers: { 'Content-Type': 'application/json' },
+//   })
+//     .then((res) => res.json())
+//     .then(json => setReports(json.report)
+//     )
+//   };
+
+// render() {
+//   const { DataisLoaded, items } = this.state;
+//   if (!DataisLoaded) return 
+//       <div>
+//     <h1> Pleses wait some time.... </h1> 
+//       </div>
+
+//   return (
+//     <section>
+//       <br/><br/><br/>
+//       <h2>Reports Edit</h2>
+//       {
+//         items.map((item) => (
+//             <form onSubmit={this.handleSubmit}>
+//                 <label><b>Report:&nbsp;</b>
+//                     <input name="photo_url" type="text" value={item.photo_url} onChange={e => setReports({...report, photo_url: e.target.value})} />
+//                 </label>
+//                 <lable>
+//                     <input name="trash_name" type="text" value={this.state.trash_name} onChange={e => setReports({...report, trash_name: e.target.value})} />
+//                 </lable>
+//                 <label> 
+//                     <input name="location" type="text" value={item.location.slice(9)} onChange={e => setReports({...report, location: e.target.value})} />
+//                 </label>
+//                 <label>
+//                     <input name="report_from" type="text" value={item.report_from.slice(2,5) + "-" + item.report_from.slice(5,8) + "-" + item.report_from.slice(8)} onChange={e => setReports({...report, report_from: e.target.value})} />
+//                 </label>
+//                 <label>
+//                     <input name="report_date" type="text" value={item.report_date.slice(0, 10)} onChange={e => setReports({...report, report_date: e.target.value})} />
+//                 </label>
                 
-                <input type="submit" value="Submit" />
-            </form>
-        ))
-        }
-        <br/><br/><br/>
-    </section> 
-  );
-}}
+//                 <input type="submit" value="Submit" />
+//             </form>
+//           ))
+//         }
+//         <br/><br/><br/>
+//     </section> 
+//   );
+// }}
 
-export default DataEdit;
+// export default DataEdit;
