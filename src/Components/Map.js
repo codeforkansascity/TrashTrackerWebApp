fef04ccc71fb241402ee845b2715e7c60d63f97f
lulu <lulu.cao.her@gmail.com>
2022-11-06 13:42:59 -0600

    added markers and popup to map

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,5 +1,6 @@
-import React from "react";
-import { MapView, LocationSearch } from "@aws-amplify/ui-react";
+import { React, useState} from "react";
+import { MapView, LocationSearch, Heading, Text, Button } from "@aws-amplify/ui-react";
+import { Marker, Popup } from 'react-map-gl';
 import "@aws-amplify/ui-react/styles.css";
 import "./Map.css";
 
@@ -9,78 +10,56 @@ import "./Map.css";
 // Currently using Navigation style;
 // Haven't tried other style
 
-const Map = () => {
+// Reference: https://ui.docs.amplify.aws/react/connected-components/geo
+// See the section on Usage with react-map-gl
+
+function MarkerWithPopup({ latitude, longitude, heading, location }) {
+  const [showPopup, setShowPopup] = useState(false);
+
+  const handleMarkerClick = ({ originalEvent }) => {
+    originalEvent.stopPropagation();
+    setShowPopup(true);
+  };
+
   return (
-    <MapView
-      id="custom-map"
-      initialViewState={{
-        latitude: 39.1002489,
-        longitude: -94.5340805,
-        zoom: 14,
-      }}
-    >
-      <LocationSearch position="top-left" />
-    </MapView>
+    <>
+      <Marker
+        latitude={latitude}
+        longitude={longitude}
+        onClick={handleMarkerClick}
+      />
+      {showPopup && (
+        <Popup
+          latitude={latitude}
+          longitude={longitude}
+          offset={{ bottom: [0, -40] }}
+          onClose={() => setShowPopup(false)}
+        >
+          <Heading level={6}>{heading}</Heading>
+          <Text>Location: {location}</Text>
+        </Popup>
+      )}
+    </>
   );
-};
+}
 
-export default Map;
+export default function MapWithMarkerPopup() {
+  const [{ latitude, longitude }, setMarkerLocation] = useState({
+    latitude: 39.1002489,
+    longitude: -94.4940805,
+  });
 
-// Reference: https://aws.amazon.com/blogs/mobile/add-maps-to-your-app-in-3-steps-with-aws-amplify-geo/
-// Reference: https://docs.amplify.aws/lib/geo/maps/q/platform/js/#display-a-map
-// Configuration for below not completed; DO NOT USE
+  const updateMarker = () => setMarkerLocation({ latitude: latitude + 0.03, longitude: longitude + 0.05 });
 
-// import { createMap } from "maplibre-gl-js-amplify";
-// import "maplibre-gl/dist/maplibre-gl.css";
-
-// import { useEffect } from 'react';
-// import { drawPoints } from "maplibre-gl-js-amplify";
-// import './Map.css';
-// import "maplibre-gl-js-amplify/dist/public/amplify-map.css";
-
-// async function initializeMap() {
-//     const map = await createMap({
-//         container: "map", // An HTML Element or HTML element ID to render the map in https://maplibre.org/maplibre-gl-js-docs/api/map/
-//         center: [-123.1187, 49.2819], // [Longitude, Latitude]
-//         zoom: 11,
-//     });
-//     return map;
-// }
-
-// initializeMap();
-
-// export default function Map() {
-//     useEffect( async () => {
-//        const map = await initializeMap();
-//      }, []);
-//      return (
-//        <div>
-//          <div id="map"></div>
-//        </div>
-//      );
-//    }
-
-// // map.on("load", function () {
-// //     drawPoints("mySourceName", // Arbitrary source name
-// //         [
-// //             {
-// //               coordinates: [-122.483696, 37.833818], // [Longitude, Latitude]
-// //               title: "Golden Gate Bridge",
-// //               address: "A suspension bridge spanning the Golden Gate",
-// //             },
-// //             {
-// //               coordinates: [- 122.4770, 37.8105], // [Longitude, Latitude]
-// //             },
-// //         ], // An array of coordinate data, an array of Feature data, or an array of [NamedLocations](https://github.com/aws-amplify/maplibre-gl-js-amplify/blob/main/src/types.ts#L8)
-// //         map,
-// //         {
-// //             showCluster: true,
-// //             unclusteredOptions: {
-// //                 showMarkerPopup: true,
-// //             },
-// //             clusterOptions: {
-// //                 showCount: true,
-// //             },
-// //         }
-// //     );
-// // });
+  return (
+    <>
+      <MapView id="custom-map" initialViewState={{ latitude: 39.1002489, longitude: -94.5340805, zoom: 10 }}>
+        <LocationSearch position="top-left" />
+        <Marker latitude={latitude} longitude={longitude} />
+        <MarkerWithPopup latitude={39.1002489} longitude={-94.5340805} heading={"Fridge"} location={"Loose Park"} />
+        <MarkerWithPopup latitude={39.1302489} longitude={-94.4940805} heading={"Mattress"} location={"8th and main st"} />
+      </MapView>
+      <Button onClick={updateMarker}>Move Marker</Button>
+    </>
+  );
+}
